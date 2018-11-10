2e01b8c02935ae836df4feb37a0a87911f2e16d9
jraleman <jraleman@bendiburg.com>
2018-11-10 05:23:48 -0800

    Add RootContainer

---
+++ src/containers/RootContainer/RootContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class RootContainer extends React.Component {
+  constructor (props) {
+    super(props);
+    return ;
+  }
+  render () {
+    return (
+      <React.Fragment>
+        <div>{ "RootContainer" }</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default RootContainer;
