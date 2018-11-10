dcd63bd975df1f3691dfe95c2ac5da3a77dadc52
jraleman <jraleman@bendiburg.com>
2018-11-10 05:24:27 -0800

    Restructure app directory tree

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+import RootContainer from './containers/RootContainer';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    return ;
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Component>
+        <RootContainer />
+      </React.Component>
     );
   }
 }
